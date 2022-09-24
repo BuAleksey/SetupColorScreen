aac2de68ad6542e85baa240330efd72aeebe0cfe
Aleksey <oldf03@gmail.com>
2022-09-24 19:29:36 +0300

    Final

--- SetupColorScreen/ViewController.swift
+++ SetupColorScreen/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var resultView: UIView!
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultView.layer.cornerRadius = 20
+        resultView.backgroundColor = .gray
     }
 
-
+    @IBAction func slidersAction() {
+        resultView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value / 255),
+            green: CGFloat(greenSlider.value / 255),
+            blue: CGFloat(blueSlider.value / 255), alpha: 1
+        )
+        redValueLabel.text = String(format: "%.2f", redSlider.value / 255)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value / 255)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value / 255)
+    }
 }
 
