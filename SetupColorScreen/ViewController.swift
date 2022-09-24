78fc3f263d140c4ca48443a2bf11a73f6cdcb857
Aleksey <oldf03@gmail.com>
2022-09-24 15:00:56 +0300

    Initial Commit

---
+++ SetupColorScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SetupColorScreen
+//
+//  Created by Buba on 24.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
